807b3f3313aee548a65e881f60d9af8cd8d4ce1d
allenmadd <32689944+allenmadd@users.noreply.github.com>
2019-01-12 18:32:52 -0800

    Add files via upload

--- generate_random_table.py
+++ generate_random_table.py
@@ -1,20 +1,19 @@
-# generates a random table of data
-# call this once at the beginning of the game and save it as a variable which can be accessed by the function calculate_score()
+#generates a random table of data
+#call this once at the beginning of the game and save it as a variable which can be accessed by the function calculate_score()
 
-import numpy as np
-
+import numpy as np 
 
 def generate_random_table():
-    a = np.random.normal(loc=1.0, scale=.8, size=(4, 4))
+	a = np.random.normal(loc=1.0, scale=.8, size= (4,4))
 
-    # turn negatives into positives and make overpowered numbers more rare
-    for i in np.arange(4):
-        for j in np.arange(4):
-            if a[i, j] < 0:
-                a[i, j] *= -1.00
-            if a[i, j] > 1.5:
-                a[i, j] -= .500
-    # print(a)
-    return a
+	#turn negatives into positives and make overpowered numbers more rare
+	for i in np.arange(4):
+		for j in np.arange(4):
+			if a[i,j] <0:
+				a[i,j]*=-1.00
+			if a[i,j] >.5 and a[i,j] <1.0:
+				a[i,j]= 1.00
+	print(a)
+	return a
 
-# generate_random_table()
+generate_random_table()
