a19d14572c8851a1f168b40dac4f913c8e2e225b
Ada Toydemir <toydemir@usc.edu>
2019-01-12 22:18:34 -0800

    Update find_max.py
    Added a recursive DFS optimal path finder to power the sms-based feedback system

--- find_max.py
+++ find_max.py
@@ -1,5 +1,6 @@
 from math import sqrt
 import numpy as np
+from calculate_score import calculate_score
 
 '''Uses Breadth First Search across all nodes to
 calculate the optimal value of the given table'''
@@ -21,3 +22,18 @@ def find_max(val_array):
 		if (cur_value > max_value):
 			max_value = cur_value
 	return (max_value)
+
+def find_path_helper(val_array, str):
+	if (str.size = int(sqrt(val_array.size))):
+		real_max = find_max(val_array)
+		cur_max = calculate_score(str, val_array)
+		if (real_max == cur_max):
+			return str
+	find_path_helper(val_array, str+"A")
+	find_path_helper(val_array, str+"B")
+	find_path_helper(val_array, str+"C")
+	find_path_helper(val_array, str+"D")
+
+def find_path(val_array):
+	str = ""
+	return find_path_helper(val_array, str)
